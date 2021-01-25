37077050a6d46be568eedaa5f93eb5db9fe58868
Kyle Goldbeck <kyle.goldbeck@gmail.com>
2021-01-24 18:06:56 -0800

    Started chip8.c

--- src/Chip-8.c
+++ src/Chip-8.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <SDL/SDL.h>
 
-void
-C8_loadFile(char* fileName){
+
+Chip8*
+chip8_init(){
+        // Load fontset into memory
+        // initialize stack, registers and graphics with memset
+        // Initialize pc = 0x200, sp &= 0, opcode = 0x200
         
 }
+
+void
+chip8_loadRom(chip*c8, char* romPath){
+        FILE* rom = fopen(romPath, "rb");
+        if (rom == NULL){
+                printf("Error loading game rom!");
+                exit(1);
+        }
+
+        fread(c8->memory[i], MEMSIZE - 0x200, rom);
+}
+
+void
+chip8_draw(Chip8* c8){
+        
+}
+
+void
+chip8_emulateCycle(Chip8* c8){
+        
+}
+
+void chip8_processInput(SDL_Event* event){
+        
+}
+        
